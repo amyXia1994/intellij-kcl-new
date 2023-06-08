7ab10df4d81d6cb19b6e8282e831f761850c3888
xiarui.xr <xiarui.xr@antgroup.com>
2023-06-08 14:55:39 +0800

    add bnf and generated files

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ plugins {
     alias(libs.plugins.kover) // Gradle Kover Plugin
 }
 
+sourceSets["main"].java.srcDirs("src/main/gen")
+
 group = properties("pluginGroup").get()
 version = properties("pluginVersion").get()
 
